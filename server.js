0cb3fa4b99b77021f5a8ec22cbfc939a1b90eac7
calteen <pjay32547@gmail.com>
2024-03-09 21:44:23 +0530

    node

---
+++ server.js
@@ -0,0 +1,70 @@
+let express = require('express')
+let bodyParser = require('body-parser')
+let path = require('path')
+let multer = require('multer')
+let docxtopdf = require('docx-pdf')
+const PDFDocument = require('pdfkit');
+const fs = require('fs');
+const imgToPDF = require('image-to-pdf')
+app = express()
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.json())
+app.use(express.static('uploads'))
+
+
+let storage = multer.diskStorage({
+    destination: function(req,file,cb){
+        cb(null,"uploads");
+    },
+    filename: function(req,file,cb){
+        cb(null, Date.now()+path.extname(file.originalname))
+    }
+})
+
+var upload = multer({storage:storage})
+
+app.get('/',(req,res)=>{
+    res.sendFile(__dirname +'/web/index.html')
+})
+
+app.post('/nodedocxpdf',upload.single('file'),(req,res)=>{
+    console.log(req.file.path)
+    try {
+        let outputfilepath ='uploads/'+ Date.now()+"output.pdf"
+        docxtopdf(req.file.path,outputfilepath,(err,result)=>{
+            if(err){
+                res.json({'filetype':'not def'})
+            }else{
+                res.download(outputfilepath)
+            }
+        })
+    } catch (error) {
+        res.json({'filetype':'not def'})
+    }
+
+
+})
+
+
+app.post('/nodejpgpdf',upload.single('file'),async (req,res)=>{
+    try {
+        let outputfilepath ='uploads/'+ Date.now()+"output.pdf"
+    const pages = [
+        fs.readFileSync(req.file.path) // Buffer
+    ]
+    m = await imgToPDF(pages, imgToPDF.sizes.A4).pipe(fs.createWriteStream(outputfilepath))
+    await new Promise(resolve => setTimeout(resolve, 1000));
+    res.download(outputfilepath)
+    } catch (error) {
+        res.json({'filetype':'not def'})
+    }
+    
+    
+    
+
+
+})
+
+app.listen(3000,()=>{
+    console.log('listening on port 3000')
+})
